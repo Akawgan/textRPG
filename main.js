e32b1ba30d5f75345850846908efaf0613a9384e
Savien <akawgan@gmail.com>
2021-07-12 22:58:29 +0200

    Add switch for changing avatar animation - test of changing classes from JS

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+//JAC
 function talkTo() {
     var element = document.getElementById("avatar-hero");
-    element.classList.add("stance-fight");
+
+    if(!element.classList.contains("stance-fight"))
+    {
+        element.classList.add("stance-fight");
+    }
+    else
+    {
+        element.classList.remove("stance-fight");
+    }
+
+
+
+
+    
+    
 }
