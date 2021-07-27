60af219a31a4e5e2779011618940ca106da17f00
Savien <akawgan@gmail.com>
2021-07-27 23:20:08 +0200

    Read map data from a separate .js file

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var currentMap = 0;
+
 //JAC
 function talkTo() {
     var element = document.getElementById("avatar-hero");
@@ -5,17 +7,24 @@ function talkTo() {
 
     if(!element.classList.contains("stance-fight"))
     {
+        currentMap = 0;
         element.classList.add("stance-fight");
     }
     else
     {
+        currentMap = 1;
         element.classList.remove("stance-fight");
+        data[0].mapText = "This is now a new description of first room! It should reset on page refresh."
     }
+
+    // Test loading map text from JS
+    var mapDescription = data[currentMap].mapText;
+
     // Scroll the main window down
     var mainStoryArea = document.getElementById("mainArea");
 
     var node = document.createElement("P");
-    var textnode = document.createTextNode("Newest Line now!");
+    var textnode = document.createTextNode(mapDescription);
     node.appendChild(textnode);
     mainStoryArea.appendChild(node);
 
