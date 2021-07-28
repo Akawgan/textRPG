a35557d8868cb7e3f509231a0a9a3f5625d0c1eb
Savien <akawgan@gmail.com>
2021-07-28 15:31:01 +0200

    Add the test map JS file

---
+++ test-map.js
@@ -0,0 +1,10 @@
+data = [
+    {
+        "mapID": "1",
+        "mapText": "You're in the room number one. It's a room with a floor, walls and even a ceiling!"
+    },
+    {
+        "mapID": "2",
+        "mapText": "Now you're in the room number two. It doesn't have any walls!"
+    }
+];
