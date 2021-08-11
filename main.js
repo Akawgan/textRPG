739cd9e9a56a52ef859f43cd84783b6cd7863564
Savien <akawgan@gmail.com>
2021-08-11 21:08:59 +0200

    Load map data from JSON

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var isLogicPaused = false;
+var currentMap;
 
 const player =
 {
@@ -40,35 +41,72 @@ function playAnimation(anim, target)
     setTimeout(() => {  element.classList.remove(anim); isLogicPaused = false; }, 990);
 }
 
-
-// ====================================================================================================
-// Button Event Listeners
-// ====================================================================================================
-function talkTo() // Test Button
+function displayText(string) // DISPLAYS A NEW TEXT ENTRY IN THE MAIN STORY WINDOW
 {
-    if(isLogicPaused) return;
-
-    navigator.vibrate(200);
-    playAnimation(1);
-
-    // Test loading map text from JS
-    var mapDescription = data[player.currentMap].mapText;
-
-    // Scroll the main window down
     var mainStoryArea = document.getElementById("mainArea");
 
     var node = document.createElement("P");
-    var textnode = document.createTextNode(mapDescription + " " + player.name);
-    node.appendChild(textnode);
+    node.innerHTML = string;
     mainStoryArea.appendChild(node);
 
     scrollStoryArea();
 }
 
+function loadJSON(callback) {   // COPYPASTE STUFF, TRY IF IT WORKS THEN DISSECT.
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'map1.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+function initializeMap(loadedObject)
+{
+    currentMap = loadedObject;
+    displayText(currentMap.description + "<br><br>" + "You're on map number <span style='color:red'>" + currentMap.mapID + "</span>");
+    console.log(currentMap);
+}
+
+// ====================================================================================================
+// Button Event Listeners
+// ====================================================================================================
+
 // BUTTONS
+function btnInitializeMap() // Map loading test. This will run when the player changes the map.
+{
+    if(isLogicPaused) return;
+
+    // Vibrate and play the animation
+    navigator.vibrate(200);
+    playAnimation("anim-run");
+
+    loadJSON(function(response) {
+          var loadedObject = JSON.parse(response); // Parse JSON string into object
+          initializeMap(loadedObject); // Initialize the map object using the newly loaded data
+    });
+}
+function btnLook() //
+{
+    if(isLogicPaused) return;
+    displayText('You look around.')
+
+    // Read map description from a JSON
+
+    navigator.vibrate(200);
+    playAnimation("anim-look");
+
+    scrollStoryArea();
+}
 function btnAttack() // Test Button
 {
     if(isLogicPaused) return;
+    displayText('You attack the <span style="color:red">enemy.</span>')
 
     navigator.vibrate(200);
     playAnimation("anim-attack");
@@ -79,6 +117,8 @@ function btnRun() // Test Button
 {
     if(isLogicPaused) return;
 
+    displayText("You move to the next map.")
+
     navigator.vibrate(200);
     playAnimation("anim-run");
 
@@ -88,19 +128,10 @@ function btnChest() // Test Button
 {
     if(isLogicPaused) return;
 
+    displayText("You open the chest.")
+
     navigator.vibrate(200);
     playAnimation("anim-chest");
-
-    scrollStoryArea();
-}
-function btnLook() // Test Button
-{
-    if(isLogicPaused) return;
-
-    // Read map description from a JSON
-
-    navigator.vibrate(200);
-    playAnimation("anim-look");
 
     scrollStoryArea();
 }
